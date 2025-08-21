796cfd4165c430d4afec6361f3272f0f5e4c4e66
Đức Trần <tranaduc9x@gmail.com>
2025-08-21 22:40:26 +0700

    solve problem 162

---
+++ binary_search/162/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func findPeakElement(nums []int) int {
+	l, r := 0, len(nums)-1
+
+	for l < r {
+		mid := l + (r-l)/2
+
+		if nums[mid] < nums[mid+1] {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+
+	return l
+}
+
+// find peak element
+// pattern binary search
+func main() {
+	nums := []int{1, 2, 3, 1}
+	fmt.Println(findPeakElement(nums))
+}
