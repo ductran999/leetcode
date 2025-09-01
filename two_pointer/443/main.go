b8bc6a4c1076906cd45508444f17ee73c25da579
Đức Trần <tranaduc9x@gmail.com>
2025-09-01 20:31:10 +0700

    solve 443

---
+++ two_pointer/443/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func compress(chars []byte) int {
+	place := 0
+
+	if len(chars) <= 1 {
+		return len(chars)
+	}
+
+	slow := 0
+	for fast := 1; fast <= len(chars); fast++ {
+		// detect last element of array or current char is different from the previous
+		if fast == len(chars) || chars[fast] != chars[fast-1] {
+			times := fast - slow
+
+			ch := chars[slow]
+			chars[place] = ch
+			place++
+
+			// number compress
+			if times > 1 {
+				// one digit
+				if times < 10 {
+					chars[place] = byte(times + '0')
+					place++
+				} else {
+					digits := []byte(strconv.Itoa(times))
+					for _, d := range digits {
+						chars[place] = d
+						place++
+					}
+				}
+			}
+
+			// update pivot for new array
+			slow = fast
+		}
+	}
+
+	return place
+}
+
+func main() {
+	chars := []byte{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}
+	fmt.Println(compress(chars))
+}
