00e988696da5ae5e8df36e348f8608cb68749374
Đức Trần <tranaduc9x@gmail.com>
2025-09-07 20:20:32 +0700

    solved 287

---
+++ array/287/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func findDuplicate(nums []int) int {
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			return nums[i]
+		}
+	}
+
+	return 0
+}
+
+func main() {
+	nums := []int{1, 3, 4, 2, 2}
+	fmt.Println(findDuplicate(nums))
+}
