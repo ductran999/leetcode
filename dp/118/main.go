a837aea16335ee87004a16fdfdd1e0d6d954bdbc
Đức Trần <tranaduc9x@gmail.com>
2025-09-12 16:43:18 +0700

    solve 118

---
+++ dp/118/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func generate(numRows int) [][]int {
+	ans := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		row := make([]int, i+1)
+		row[0], row[i] = 1, 1
+
+		for j := 1; j < i; j++ {
+			row[j] = ans[i-1][j-1] + ans[i-1][j]
+		}
+
+		ans[i] = row
+	}
+	return ans
+}
+
+func main() {
+	numRows := 5
+	fmt.Println(generate(numRows))
+}
