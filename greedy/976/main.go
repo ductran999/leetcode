740b9d8c10a0ced259b71f54241dfcd12198764d
Đức Trần <tranaduc9x@gmail.com>
2025-09-28 08:45:48 +0700

    solve 976

---
+++ greedy/976/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func largestPerimeter(nums []int) int {
+	sort.Ints(nums)
+	for i := len(nums) - 1; i > 1; i-- {
+		if nums[i] < (nums[i-1] + nums[i-2]) {
+			return (nums[i] + nums[i-1] + nums[i-2])
+		}
+	}
+	return 0
+}
+
+func main() {
+	nums := []int{2, 1, 2}
+	fmt.Println(largestPerimeter(nums))
+}
